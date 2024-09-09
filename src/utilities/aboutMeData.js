96eb557e6f79c18626cc84916f780cb027d3bfe3
MargretPetrova <margret.ruseva@gmail.com>
2024-09-09 09:06:53 +0300

    About me changes

--- src/utilities/aboutMeData.js
+++ src/utilities/aboutMeData.js
@@ -1,5 +1,5 @@
 const SCREEN_CONSTANTS = { 
-    description: "Positive, organized and hard working person, looking for a junior JS position and opportunity to grow my skills , either in frontend or in backend . Able to work well under pressure.",
+    description: "Positive, organized and hard working frond-end developer with hands-on experience in React, Redux, TypeScript and AngularJS. Skilled in working with monorepo architectures and familiar with Agile methodologies, including acting as Scrum master on a rotating basis within a cross-functional team. Looking for a JS position and opportunity to grow my skills , either in frontend or in backend . Able to work well under pressure.",
     highlights: {
         bullets: [
             "Hard-working",
